6eab33733231ddb127c18236b416ba1fa916f843
Avichal Trivedi <avichaltrivedi14@gmail.com>
2024-08-06 15:10:14 +0530

    components

---
+++ healthnet/app/home/page.tsx
@@ -0,0 +1,12 @@
+import { HomePage } from '@/components/component/home-page';
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
+}
+
+export default page;
